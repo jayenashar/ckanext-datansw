9447e3a6de80a2aabda96c7806e7360a61684956
Mark Gregson <mark.gregson@linkdigital.com.au>
2017-06-28 15:46:12 +1000

    Add saml2 configuration

---
+++ config/saml2/attributemaps/basic.py
@@ -0,0 +1,11 @@
+MAP = {
+    "identifier": "urn:oasis:names:tc:SAML:2.0:attrname-format:basic",
+    "fro": {
+        'Email': 'email',
+        'ID': 'id',
+        'DisplayName': 'displayName',
+        'Tenancy': 'tenancy'
+    }
+    "to": {
+    }
+}
